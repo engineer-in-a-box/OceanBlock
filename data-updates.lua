65c95c2be845e2c5440863e64749aa88f5d11bd6
engineer-in-a-box <157236565+engineer-in-a-box@users.noreply.github.com>
2025-03-21 21:26:14 +0000

    Update data-updates.lua

--- data-updates.lua
+++ data-updates.lua
@@ -21,12 +21,3 @@ data.raw["recipe"]["valves-overflow"].ingredients = {
     {type = "item", name = "iron-gear-wheel", amount = 1},
     {type = "item", name = "pipe", amount = 1}
 }
-
-if mods["BetterBelts"] then
-    data.raw["transport-belt"]["BetterBelts_ultra-transport-belt"].speed = 0.125
-    data.raw["underground-belt"]["BetterBelts_ultra-underground-belt-v1"].speed = 0.125
-    data.raw["underground-belt"]["BetterBelts_ultra-underground-belt-v2"].speed = 0.125
-    data.raw["underground-belt"]["BetterBelts_ultra-underground-belt-v3"].speed = 0.125
-    data.raw["splitter"]["BetterBelts_ultra-splitter"].speed = 0.125
-    data.raw["loader"]["BetterBelts_ultra-loader"].speed = 0.125
-end
