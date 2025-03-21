221f0712e3f95dfd30bf77c1461c865ba8188958
engineer-in-a-box <157236565+engineer-in-a-box@users.noreply.github.com>
2025-03-21 19:06:31 +0000

    Added ore productivity

--- scripts/technologies.lua
+++ scripts/technologies.lua
@@ -62,3 +62,133 @@ technologies["mining-productivity-4"].hidden = true
 technologies["landfill"].hidden = true
 
 table.insert(technologies["processing-unit"].effects, {type = "unlock-recipe", recipe = "advanced-processing-unit"})
+
+data:extend({
+    {
+        type = "technology",
+        name = "ore-productivity-1",
+        effects = {
+            {type = "change-recipe-productivity", recipe = "mineral-water-crystalization", change = 0.1}
+        },
+        icons = {
+            {icon = "__base__/graphics/icons/iron-ore.png", shift = {-1, -0}, scale = 2},
+            {
+              icon = "__core__/graphics/icons/technology/constants/constant-mining-productivity.png",
+              icon_size = 128,
+              scale = 0.5,
+              shift = {50, 50}
+            }
+        },
+        unit = {
+            ingredients = {
+                {"automation-science-pack", 1},
+                {"logistic-science-pack", 1}
+            },
+            time = 30,
+            count = 250
+        },
+        prerequisites = {
+            "logistic-science-pack",
+            "automation"
+        }
+    },
+    {
+        type = "technology",
+        name = "ore-productivity-2",
+        effects = {
+            {type = "change-recipe-productivity", recipe = "mineral-water-crystalization", change = 0.1},
+            {type = "change-recipe-productivity", recipe = "slurry-filtering-1", change = 0.1},
+            {type = "change-recipe-productivity", recipe = "slurry-filtering-2", change = 0.1}
+        },
+        icons = {
+            {icon = "__base__/graphics/icons/iron-ore.png", shift = {-1, -0}, scale = 2},
+            {
+              icon = "__core__/graphics/icons/technology/constants/constant-mining-productivity.png",
+              icon_size = 128,
+              scale = 0.5,
+              shift = {50, 50}
+            }
+        },
+        unit = {
+            ingredients = {
+                {"automation-science-pack", 1},
+                {"logistic-science-pack", 1},
+                {"chemical-science-pack", 1}
+            },
+            time = 30,
+            count = 500
+        },
+        prerequisites = {
+            "ore-productivity-1",
+            "chemical-science-pack"
+        }
+    },
+    {
+        type = "technology",
+        name = "ore-productivity-3",
+        effects = {
+            {type = "change-recipe-productivity", recipe = "mineral-water-crystalization", change = 0.1},
+            {type = "change-recipe-productivity", recipe = "slurry-filtering-1", change = 0.1},
+            {type = "change-recipe-productivity", recipe = "slurry-filtering-2", change = 0.1},
+            {type = "change-recipe-productivity", recipe = "ferric-solution-filtering-2", change = 0.1},
+            {type = "change-recipe-productivity", recipe = "cupric-solution-filtering-2", change = 0.1}
+        },
+        icons = {
+            {icon = "__base__/graphics/icons/iron-ore.png", shift = {-1, -0}, scale = 2},
+            {
+              icon = "__core__/graphics/icons/technology/constants/constant-mining-productivity.png",
+              icon_size = 128,
+              scale = 0.5,
+              shift = {50, 50}
+            }
+        },
+        unit = {
+            ingredients = {
+                {"automation-science-pack", 1},
+                {"logistic-science-pack", 1},
+                {"chemical-science-pack", 1},
+                {"production-science-pack", 1}
+            },
+            time = 30,
+            count = 1000
+        },
+        prerequisites = {
+            "ore-productivity-2",
+            "production-science-pack"
+        }
+    },
+    {
+        type = "technology",
+        name = "ore-productivity-4",
+        effects = {
+            {type = "change-recipe-productivity", recipe = "mineral-water-crystalization", change = 0.1},
+            {type = "change-recipe-productivity", recipe = "slurry-filtering-1", change = 0.1},
+            {type = "change-recipe-productivity", recipe = "slurry-filtering-2", change = 0.1},
+            {type = "change-recipe-productivity", recipe = "ferric-solution-filtering-2", change = 0.1},
+            {type = "change-recipe-productivity", recipe = "cupric-solution-filtering-2", change = 0.1}
+        },
+        icons = {
+            {icon = "__base__/graphics/icons/iron-ore.png", shift = {-1, -0}, scale = 2},
+            {
+              icon = "__core__/graphics/icons/technology/constants/constant-mining-productivity.png",
+              icon_size = 128,
+              scale = 0.5,
+              shift = {50, 50}
+            }
+        },
+        unit = {
+            ingredients = {
+                {"automation-science-pack", 1},
+                {"logistic-science-pack", 1},
+                {"chemical-science-pack", 1},
+                {"production-science-pack", 1}
+            },
+            time = 60,
+            count_formula = "1000*(L-2)"
+        },
+        prerequisites = {
+            "ore-productivity-3"
+        },
+        max_level = "infinite"
+    }
+})
