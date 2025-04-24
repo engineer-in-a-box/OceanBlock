bcd19368238e23b4705442065e41b7c365b85ebb
engineer-in-a-box <157236565+engineer-in-a-box@users.noreply.github.com>
2025-04-24 03:20:38 +0000

    Delete technologies.lua

--- technologies.lua
+++
@@ -1,9 +0,0 @@
-local tech = data.raw["technology"]
-tech["foundation"].prerequisites = {"tungsten-steel"}
-table.insert(tech["foundry"].effects, {type = "unlock-recipe", recipe = "stone-from-lava"})
-table.insert(tech["foundry"].effects, {type = "unlock-recipe", recipe = "tungstic-acid"})
-table.insert(tech["foundry"].effects, {type = "unlock-recipe", recipe = "tungsten-ore"})
-table.insert(tech["foundry"].effects, {type = "unlock-recipe", recipe = "casting-titansteel-plate"})
-table.insert(tech["foundry"].effects, {type = "unlock-recipe", recipe = "calcite"})
-table.insert(tech["planet-discovery-vulcanus"].effects, {type = "unlock-recipe", recipe = "vulcanus-scavenging"})
-table.insert(tech["planet-discovery-vulcanus"].effects, {type = "unlock-recipe", recipe = "vulcanus-air-separation"})
