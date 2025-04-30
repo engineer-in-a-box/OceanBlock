42fef2da2a0c302c5442456b95c971ff0de93f08
Engineer in a box <157236565+engineer-in-a-box@users.noreply.github.com>
2025-04-30 01:58:07 +0000

    added sulfur to results of huge corrundum rock

--- prototypes/compat/corrundum/entities.lua
+++ prototypes/compat/corrundum/entities.lua
@@ -1,1 +1,2 @@
 table.insert(data.raw["simple-entity"]["huge-corrundum-rock"].minable.results, {type = "item", name = "calcite", amount_min = 1, amount_max = 5})
+table.insert(data.raw["simple-entity"]["huge-corrundum-rock"].minable.results, {type = "item", name = "sulfur", amount_min = 1, amount_max = 5})
